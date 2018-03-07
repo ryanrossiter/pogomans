de7f6471c2950381678bd21112261fc561d3f542
Ryan Rossiter <nayr0788@gmail.com>
2018-03-06 19:41:31 -0500

    Fix for other screen resolutions

--- src/player.js
+++ src/player.js
@@ -1,7 +1,7 @@
 import game from './game';
 import Defs from './defs';
 
-const MAX_SPEED = 350;
+const MAX_SPEED = Defs.GAME_HEIGHT / 3;
 const TARGET_Y = 200;
 
 export default class Player {
@@ -17,7 +17,7 @@ export default class Player {
 
         let speedFac = (pointerX - Defs.GAME_WIDTH / 2) * 3 / Defs.GAME_WIDTH;
         let speedXFac = Math.sign(speedFac) * Math.min(Math.abs(speedFac), 0.95);
-        let speedYFac = -Math.sqrt(1 - Math.pow(speedXFac, 2)) * 0.9;
+        let speedYFac = -Math.sqrt(1 - Math.pow(speedXFac, 2)) * 0.80;
         this.sprite.position.x += game.time.elapsed / 1000 * speedXFac * MAX_SPEED;
         this.sprite.position.y += game.time.elapsed / 1000 * speedYFac * MAX_SPEED;
     }
